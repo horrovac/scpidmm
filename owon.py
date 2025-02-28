0757e13c525664ce7a92470cf23ca05c69079556
horrovac <faruk.kujundzic@jku.at>
2025-02-28 08:26:03 +0100

    commit messages

--- owon.py
+++ owon.py
@@ -8,26 +8,8 @@ import time
 class DMM:
 	"""Communication with a multimeter"""
 
-    # NAME:[button_index, "display unit", index]
-	global m
-	m = {
-		'VOLT':		[0,"VDC",0],
-		'VOLT AC':	[1,"VAC",0],
-		'CURR':		[2,"ADC",1],
-		'CURR AC':	[3,"AAC",1],
-		'RES':		[4,"Ω",2],
-		'CONT':		[5,"Ω",2],
-		'DIOD':		[6,"V",2],
-		'CAP':		[7,"F",3],
-		'FREQ':		[8,"Hz",4],
-		'TEMP':		[9,"°C",5],
-		'OFFLINE':	[10,"OFFLINE",-1,lambda:'OFFLINE']
-	}
-	i=list(m.keys())
-
 	port='/dev/ttyUSB0'
 	measurement=0
-	global function1
 	function2=0
 	meas_range=0
 
@@ -39,7 +21,21 @@ class DMM:
 		ser.timeout=1
 		ser.open()
 		self.idn=self.query('*IDN?')
-		self.function1 = 0
+		self.m = {}
+		self.m['VOLT'] = Volt(self,0,"VDC")
+		self.m['VOLT AC'] = Volt(self,1,"VAC")
+#		'VOLT':		[0,"VDC",0],
+#		'VOLT AC':	[1,"VAC",0],
+#		'CURR':		[2,"ADC",1],
+#		'CURR AC':	[3,"AAC",1],
+#		'RES':		[4,"Ω",2],
+#		'CONT':		[5,"Ω",2],
+#		'DIOD':		[6,"V",2],
+#		'CAP':		[7,"F",3],
+#		'FREQ':		[8,"Hz",4],
+#		'TEMP':		[9,"°C",5],
+#		'OFFLINE':	[10,"OFFLINE",-1,lambda:'OFFLINE']
+#	self.function1 = "foo"
 
 	def query(self, query):
 		msg="{}\n".format(query).encode('utf-8')
@@ -54,7 +50,8 @@ class DMM:
 			self.function1 = m['OFFLINE'][0]
 			self.measurement = 0
 		else:
-			self.function1 = m[function1][0]
+			self.function1 = self.m[function1]
+			self.func1 = self.function1
 			msg=("MEAS?\n".encode('utf-8'))
 			ser.write(msg)
 			result=ser.readline().decode('utf-8')
@@ -71,26 +68,14 @@ class DMM:
 			self.measurement=float(num) * pow(10,int(exp))
 	
 	def value(self):
-		return m[self.i[self.function1]][3]()
-		measurement = self.measurement
-		function1 = self.function1
-		if (function1 == m['OFFLINE'][0] ):
-			return 'OFFLINE'
-
-		if (measurement < 0.0000001 ):
-			retval = "{:06.2f} {}".format(measurement * 1e9, "n")
-		elif (measurement < 0.0001 ):
-			retval = "{:06.3f} {}".format(measurement * 1000000, "µ")
-		elif (measurement < 0.1 ):
-			retval = "{:06.3f} {}".format(measurement * 1000, "m")
-		return retval
+		return self.func1
 
 	def switch_mode(self, mode):
 		msg=("CONF:{}".format((mode)))
 		ser.write((msg+"\n").encode('utf-8'))
 		DEBUG and print ( msg )
 
-class Mode:
+class Function:
 	"""class representing current mode of the multimeter"""
 
 	def __init__(self, p, mode, unit):
@@ -98,14 +83,62 @@ class Mode:
 		self.p=p
 		self.unit = unit
 
-class Cap(Mode):
+	def normalised_value(self):
+		meas = self.p.measurement
+		if (meas == 1e9):
+			retval = None
+		elif(meas > 1e6):
+			retval = "{:.4f} M".format(meas / 1e6)
+		elif(meas > 1e3):
+			retval = "{:.4f} k".format(meas / 1e3)
+		elif (meas < 1e-9):
+			retval = "{:06.2f} n".format(meas * 1e9)
+		elif (meas < 1e-6 ):
+			retval = "{:06.3f} µ".format(meas * 1e6)
+		elif (meas < 0.1 ):
+			retval = "{:06.3f} m".format(meas * 1e3)
+		else:
+			retval = "{:06.4f} ".format(meas)
+		return retval
+
+class Signed(Function):
+	def __str__(self):
+		try:
+			# if normalised_value() returns None the concatenation
+			# with # a string will throw a TypeError and return
+			# overload
+			retval = self.normalised_value()+self.unit
+		except TypeError:
+			retval = "OVERLOAD"
+		return retval
+
+class Volt(Function):
+	def __str__(self):
+		meas = self.p.measurement
+		if (meas < 1e-3):
+			retval = "{:.4f} {}".format(meas, self.unit)
+		else:
+			try:
+				# if normalised_value() returns None the concatenation
+				# with # a string will throw a TypeError and return
+				# overload
+				retval = self.normalised_value()+self.unit
+			except TypeError:
+				retval = "OVERLOAD"
+		return retval
+
+class Unsigned(Function):
 
 	def __str__(self):
 		meas = self.p.measurement
-		print ( meas )
 		if ( meas < 0 ):
-			meas = 0
-		#return "{:07.4f} {}".format(meas, self.unit)
-		return self.unit
-
-
+			return "0 "+self.unit
+		else:
+			try:
+				# if normalised_value() returns None the concatenation
+				# with # a string will throw a TypeError and return
+				# overload
+				retval = self.normalised_value()+self.unit
+			except TypeError:
+				retval = "OVERLOAD"
+			return retval
--- test.py
+++ test.py
@@ -3,15 +3,12 @@
 import owon
 from time import sleep
 
+
 mm = owon.DMM()
 
 mm.get()
 
-print ( mm.i[mm.function1] )
-print (mm.measurement)
-
-mode = owon.Cap(mm, 2, "F")
 while ( True ):
 	mm.get()
-	print (mode)
+	print (mm.value())
 	sleep(0.1)
